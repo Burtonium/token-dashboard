a8789abd2972294751f6f5b7c49acb95ebc62855
Burtonium <9215927+Burtonium@users.noreply.github.com>
2025-02-12 15:55:16 +0700

    Add sentry capture to serveractionerrors

--- apps/rwg-dashboard-ui/src/server/actions/errors.ts
+++ apps/rwg-dashboard-ui/src/server/actions/errors.ts
@@ -1,11 +1,17 @@
+import { captureMessage } from '@sentry/nextjs';
+
 export type ServerActionError = {
   type: 'ServerActionError';
   message: string;
   error: true;
 };
 
-export const constructError = (message: string): ServerActionError => ({
-  type: 'ServerActionError',
-  message,
-  error: true,
-});
+export const constructError = (message: string): ServerActionError => {
+  captureMessage(`ServerActionError: ${message}`);
+
+  return {
+    type: 'ServerActionError',
+    error: true,
+    message,
+  };
+};
